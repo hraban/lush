27b54b68e3e2804341ff2503bd4da3c0401e5f67
Hraban Luyat <hraban@0brg.net>
2014-01-31 04:16:19 +0100

    empty help tab invites programmers to help
    fixes hraban/lush#72. more or less.

--- static/js/lush/help.js
+++ static/js/lush/help.js
@@ -23,6 +23,17 @@
 
 // COMMAND WIDGET HELP ACTIONS
 
+
+// howdy! sooo... yeaahhh... it's a bit sad that this file has been getting so
+// very very little love, especially considering that this is THE solution to
+// the problem that caused the frustration out of which I started this entire
+// project to begin with (wtf-was-that-command-name-wtf-was-that-flag-syndrome).
+// ha, ha. ahum. but you know.. omg parsers!
+//
+// I guess UIs are really, really not my thing.
+//
+// anyway some mc'lovin would be much much appreciated here.
+
 define(["jquery"], function ($) {
     var actions = {
         tar: function (cmd, $help) {
@@ -55,6 +66,8 @@ define(["jquery"], function ($) {
             $help.append($('<label>extract: </label>').append($changeflag));
         },
         git: function (cmd, $help) {
+            // hahahahaha what is this my dog's funeral? because that's how much
+            // I'm crying right now.
             $help.append($('<a href="https://www.kernel.org/pub/software/scm/git/docs/" target=_blank>online man page</a>'));
             $help.append($('<br>'))
             $help.append($('<a href="">back</a>').click(function (e) {
@@ -63,7 +76,11 @@ define(["jquery"], function ($) {
         },
     };
 
+    function defaultHelp(cmd, $help) {
+        $help.html('This tab is intended for interactive management of command line flags. Currently only a proof of concept, if you happen to be a programmer willing to help me out here that would be great. Shoot me an e-mail at hraban@0brg.net or go to <a target=_blank href=http://github.com/hraban/lush>github.com/hraban/lush</a> and have a look around! Especially in the <a target=_blank href=http://github.com/hraban/lush/blob/master/static/js/lush/help.js>/static/js/lush/help.js</a> file.');
+    }
+
     return function (cmd) {
-        return actions[cmd.cmd] || null;
+        return actions[cmd.cmd] || defaultHelp;
     };
 })
