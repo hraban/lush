7bb9e8e655bae8793000ba1df6cac530c3cae10b
Hraban Luyat <hraban@0brg.net>
2014-03-24 20:12:16 +0000

    Actual websocket unit testing

--- websocket_test.go
+++ websocket_test.go
@@ -21,14 +21,19 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
-	_ "code.google.com/p/go.net/websocket"
+	"code.google.com/p/go.net/websocket"
 )
 
+// this function starts a server listening on localhost:15846 (good) but then
+// also tests web.go a little bit. That's insane, if anything it should be part
+// of web.go's unit tests, not lush.
 func makeTestServer(t *testing.T) (*server, <-chan error) {
 	s := newServer()
 	s.web.Get("/ping", func() string {
@@ -37,6 +42,7 @@ func makeTestServer(t *testing.T) (*server, <-chan error) {
 	errc := make(chan error)
 	go func() {
 		errc <- s.Run("localhost:15846")
+		close(errc)
 	}()
 	// wait arbitrary time for server to start
 	time.Sleep(time.Second)
@@ -46,6 +52,8 @@ func makeTestServer(t *testing.T) (*server, <-chan error) {
 		// forget about it.
 		panic("failed to create ping request for testing")
 	}
+	// This, obviously, is not actually testing the live connection, so it's
+	// kind of useless.
 	s.ServeHTTP(rec, req)
 	if rec.Code == 200 {
 		return s, errc
@@ -56,9 +64,36 @@ func makeTestServer(t *testing.T) (*server, <-chan error) {
 	panic("failed to create test server")
 }
 
-func TestStub(t *testing.T) {
+// helper type to leverage ioutil.ReadAll in ioutilReadMsg. (never tested this,
+// tbh, but it seems to work for small messages so I guess I'll just use it
+// until it breaks.. *sigh*)
+type messageReader struct {
+	io.Reader
+}
+
+// non-conforming because it doesn't return 0, io.EOF on repeated use
+func (mr messageReader) Read(buf []byte) (int, error) {
+	n, err := mr.Reader.Read(buf)
+	if err != nil {
+		return n, err
+	}
+	if n == len(buf) {
+		// there's more
+		return n, nil
+	}
+	return n, io.EOF
+}
+
+func ioutilReadMsg(r io.Reader) ([]byte, error) {
+	return ioutil.ReadAll(messageReader{r})
+}
+
+// Test websocket with an actual TCP connection
+func TestWebsocketLive(t *testing.T) {
 	s, weberrc := makeTestServer(t)
 	done := make(chan int)
+	// I wrote this stuff a while ago and it looks a bit convoluted / complex.
+	// Needs trimming.
 	go func() {
 		select {
 		case <-done:
@@ -69,10 +104,26 @@ func TestStub(t *testing.T) {
 			}
 		}
 	}()
+	defer func() {
+		done <- 80085
+		s.Close()
+		// drain errors, if any
+		for _ = range weberrc {
+		}
+	}()
 
-	// test server here...
-
-	done <- 80085
-	s.Close()
-	<-weberrc
+	// Open a websocket client to the listening socket
+	origin := "http://localhost:15846/"
+	url := "ws://localhost:15846/ctrl"
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		t.Fatal("Couldn't open client websocket connection: ", err)
+	}
+	buf, err := ioutilReadMsg(ws)
+	if err != nil {
+		t.Fatal("Error reading first websocket message: ", err)
+	}
+	if string(buf) != "clientid;1" {
+		t.Errorf("Unexpected websocket handshake: %v", buf)
+	}
 }
