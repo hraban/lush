4a3980370c27b567ab8585843a47147136943cf8
Hraban Luyat <hraban@0brg.net>
2014-11-06 22:23:19 +0100

    Test new config for travis-ci

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ var typescript = require('gulp-tsc');
 var gulp = require('gulp');
 
 gulp.task('default', function() {
-  // place code for your default task here
-});
-
-gulp.task('compile', function () {
     gulp.src(['typescript/utils.ts'])
         .pipe(typescript({
             module: "amd"
