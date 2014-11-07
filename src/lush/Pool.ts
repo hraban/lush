75db9980e3c8110cfe365e9dd499069414d208a0
Hraban Luyat <hraban@0brg.net>
2014-11-07 08:50:10 +0100

    port historywidget to typescript

--- src/lush/Pool.ts
+++ src/lush/Pool.ts
@@ -21,9 +21,13 @@
 // Element pool that allows consuming elements before they were added by
 // registering callbacks and calling them when an element is added.
 
+interface Callback<T> {
+    (el: T): void
+}
+
 class Pool<T> {
     private _ar: T[] = [];
-    private _pendingcbs: {(el: T): void}[] = [];
+    private _pendingcbs: Callback<T>[] = [];
 
     // store an element in the pool. if a "consume" action was pending, call it
     // immediately with this element.
@@ -39,7 +43,7 @@ class Pool<T> {
 
     // Take an element from the pool and call f on it. If the pool is empty,
     // wait until an element is available (iow: could be async).
-    consume(f: (el: T) => void) {
+    consume(f: Callback<T>) {
         var pool = this;
         if (pool._ar.length > 0) {
             var el = pool._ar.shift();
