0105120426b84301063b830f7bc4f71674f37dec
Hraban Luyat <hraban@0brg.net>
2014-11-20 04:07:41 +0100

    install jquery and jquery-ui from bower
    not sure if this is how Bower, Gulp and NPM are supposed to be
    configured, but it's a step.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
-var typescript = require('gulp-typescript');
+var bowerFiles = require('main-bower-files');
 var gulp = require('gulp');
+var typescript = require('gulp-typescript');
+var filelog = require('gulp-filelog');
 
-gulp.task('default', function() {
-    gulp.src(['src/lush/*.ts'])
+gulp.task('default', ['javascript', 'typescript', 'libs'], function () {
+    gulp.src(['build/js/*.js'])
+        .pipe(gulp.dest('static/js/lush/'));
+});
+
+gulp.task('typescript', function () {
+    return gulp.src(['src/lush/*.ts'])
         .pipe(typescript({
             module: "amd",
             declarationFiles: false
         })).js
-        .pipe(gulp.dest('static/js/lush/'));
-    gulp.src(['src/lush/*.js'])
-        .pipe(gulp.dest('static/js/lush/'));
+        .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('javascript', function () {
+    return gulp.src(['src/lush/*.js'])
+        .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('libs', function () {
+    gulp.src(bowerFiles())
+        .pipe(filelog())
+        .pipe(gulp.dest('static/js/ext/'));
 });
--- static/js/lush.js
+++ static/js/lush.js
@@ -43,8 +43,7 @@ requirejs.config({
     "baseUrl": "/js/ext",
     "paths": {
         "lush": "../lush",
-        "jquery": "jquery-2.0.3.min",
-        "jquery.ui": "jquery-ui-1.10.2.min",
+        "jquery.ui": "jquery-ui",
         "jquery.terminal": "jquery.terminal-0.8.6.min",
         "jsPlumb": "jquery.jsPlumb-1.5.3-min",
         "ansi_up": "ansi_up-r7e9940fdad",
