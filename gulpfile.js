3865f04c10e0daa25eb649983ea64a36caa7992d
Hraban Luyat <hraban@0brg.net>
2015-01-22 00:52:00 +0100

    frustration with gulp increases..

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,18 @@ var source = require('vinyl-source-stream');
 // awww already?? please, more plugins! I love spending time learning how to use
 // and configure the build step dujour instead of coding, so much!
 
+var BOWER_LIBS = [
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/jquery-ui/jquery-ui.js',
+    'bower_components/jquery.terminal/js/jquery.terminal-src.js',
+    'bower_components/eventEmitter/EventEmitter.js'
+];
+
+// like the eponymous unix tool
+function basename(path) {
+    return path.split('/').slice(-1)[0];
+}
+
 gulp.task('default', ['js', 'statics'], function () {
 });
 
@@ -17,12 +29,19 @@ gulp.task('js', ['js-src', 'typescript', 'js-libs'], function () {
     var bundle = browserify({
             debug: true,
             paths: ['./build/js'],
+            // This doesn't work and debugging this is harder than a diamond
+            // I'm so frustrated at this build process it's unbelievable. How do
+            // people not go take to the streets and start murdering the
+            // innocent? If this were my full time job I would contemplate self
+            // mutilation every. single. day.
+            noParse: BOWER_LIBS.map(basename),
         }).transform(aliasify.configure({
             aliases: {
                 "jquery.ui": "jquery-ui",
                 'jquery.terminal': 'jquery.terminal-src',
                 'ansi_up': 'ansi_up-r7e9940fdad',
                 'react': 'react-with-addons-0.12.0.min',
+                'react/addons': 'react-with-addons-0.12.0.min',
             },
         })).require("./build/js/lush/startlush.js", {expose: "lush/start"})
         .require("./build/js/lush/tests.js", {expose: "lush/tests"})
@@ -70,12 +89,7 @@ gulp.task('js-libs-local', function () {
 gulp.task('js-libs-bower', ['jquery-ui-theme'], function () {
     // tried bunch of extensions, but manual seems to be best
     // :(
-    gulp.src([
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/jquery-ui/jquery-ui.js',
-            'bower_components/jquery.terminal/js/jquery.terminal-src.js',
-            'bower_components/eventEmitter/EventEmitter.js'
-        ]).pipe(gulp.dest('build/js/'));
+    gulp.src(BOWER_LIBS).pipe(gulp.dest('build/js/'));
     gulp.src(['bower_components/jquery.terminal/css/jquery.terminal.css'])
         .pipe(gulp.dest('static/css/'));
 });
